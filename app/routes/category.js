2947b0663fc2d8c433b9067f493c2c42cc3f2722
Clara Tersi <claratersi@gmail.com>
2017-10-05 19:52:40 -0300

    refactoring

--- app/routes/category.js
+++ app/routes/category.js
@@ -2,30 +2,29 @@
 // list categories
 
 module.exports = function category(app) {
-
-  app.get('/category/list', function list(req, res) {
-    let connection = app.infra.connectionFactory();
-    let categoryDAO = new app.infra.CategoryDAO(connection);
-    categoryDAO.findAll(function callback(error, result) {
-      res.status(200).json(result);
+  app.get('/category', (request, response) => {
+    const connection = app.infra.connectionFactory();
+    const categoryDAO = new app.infra.CategoryDAO(connection);
+    categoryDAO.findAll((error, result) => {
+      response.status(200).json(result);
     });
     connection.end();
   });
 
-  app.get('/category/:id', function findById(req, res) {
-    console.log(req.param.id);
-    let connection = app.infra.connectionFactory();
-    let categoryDAO = new app.infra.CategoryDAO(connection);
-    categoryDAO.findById(req.param.id, function callback(error, result) {
-      if(error) {
-        console.log("error");
-        res.status(500).send(error);
+  app.post('/category', (request, response) => {
+    // save category
+  });
+
+  app.get('/category/:id', (request, response) => {
+    const connection = app.infra.connectionFactory();
+    const categoryDAO = new app.infra.CategoryDAO(connection);
+    categoryDAO.findById(request.params.id, (error, result) => {
+      if (error) {
+        response.status(500).send(error);
       } else {
-        console.log(result);
-        res.status(200).json(result);
+        response.status(200).json(result);
       }
     });
     connection.end();
   });
-
 };
