de6532da970af76f6b12c2841621da971ac93381
Clara Tersi <claratersi@gmail.com>
2017-10-05 19:31:03 -0300

    product route refactor

--- app/routes/product.js
+++ app/routes/product.js
@@ -4,30 +4,29 @@
 // name, description, picture, category
 
 module.exports = function products(app) {
-
-  app.get('/product/list', function list(req, res) {
-    let connection = app.infra.connectionFactory();
-    let productDAO = new app.infra.ProductDAO(connection);
-    productDAO.findAll(function callback(error, result) {
-      res.status(200).json(result);
+  app.get('/product', (request, response) => {
+    const connection = app.infra.connectionFactory();
+    const productDAO = new app.infra.ProductDAO(connection);
+    productDAO.findAll((error, result) => {
+      response.status(200).json(result);
     });
     connection.end();
   });
 
-  app.get('/product/:id', function findById(req, res) {
-    console.log(req.param.id);
-    let connection = app.infra.connectionFactory();
-    let productDAO = new app.infra.ProductDAO(connection);
-    productDAO.findById(req.param.id, function callback(error, result) {
-      if(error) {
-        console.log("error");
-        res.status(500).send(error);
+  app.post('/product', (request, response) => {
+    // save product
+  });
+
+  app.get('/product/:id', (request, response) => {
+    const connection = app.infra.connectionFactory();
+    const productDAO = new app.infra.ProductDAO(connection);
+    productDAO.findById(request.params.id, (error, result) => {
+      if (error) {
+        response.status(500).send(error);
       } else {
-        console.log(result);
-        res.status(200).json(result);
+        response.status(200).json(result);
       }
     });
     connection.end();
   });
-
 };
