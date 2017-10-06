cfc5b144ee3765e69152dc45b0c9719c6413bd20
Clara Tersi <claratersi@gmail.com>
2017-10-05 23:50:15 -0300

    category post route added

--- app/routes/category.js
+++ app/routes/category.js
@@ -1,28 +1,34 @@
-// save category
-// list categories
-
-module.exports = function category(app) {
-  app.get('/category', (request, response) => {
+module.exports = (app) => {
+  app.get('/category', (req, res) => {
     const connection = app.infra.connectionFactory();
     const categoryDAO = new app.infra.CategoryDAO(connection);
     categoryDAO.findAll((error, result) => {
-      response.status(200).json(result);
+      res.status(200).json(result);
     });
     connection.end();
   });
 
-  app.post('/category', (request, response) => {
-    // save category
-  });
-
-  app.get('/category/:id', (request, response) => {
+  app.post('/category', (req, res) => {
     const connection = app.infra.connectionFactory();
     const categoryDAO = new app.infra.CategoryDAO(connection);
-    categoryDAO.findById(request.params.id, (error, result) => {
+    categoryDAO.save(req.body, (error, result) => {
       if (error) {
-        response.status(500).send(error);
+        res.status(500).send(error);
       } else {
-        response.status(200).json(result);
+        res.status(200).json(result);
+      }
+    });
+    connection.end();
+  });
+
+  app.get('/category/:id', (req, res) => {
+    const connection = app.infra.connectionFactory();
+    const categoryDAO = new app.infra.CategoryDAO(connection);
+    categoryDAO.findById(req.params.id, (error, result) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).json(result);
       }
     });
     connection.end();
