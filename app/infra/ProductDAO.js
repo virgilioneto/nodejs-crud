0080d6ab08e55d654b223664f53cfd65e85b6956
Clara Tersi <claratersi@gmail.com>
2017-10-07 00:52:03 -0300

    find product by name

--- app/infra/ProductDAO.js
+++ app/infra/ProductDAO.js
@@ -11,6 +11,19 @@ const findAll = (cb) => {
   });
 };
 
+const findByParam = (name, cb) => {
+  sequelize.connection.sync().then(() => {
+    sequelize.Product.findAll({
+      attributes: ['id', 'name'],
+      where: {
+        name,
+      },
+    }).then((products) => {
+      cb(JSON.parse(JSON.stringify(products)));
+    });
+  });
+};
+
 const save = (product, cb) => {
   sequelize.Product.findOne({
     where: {
@@ -72,50 +85,5 @@ module.exports = {
   findAll,
   findById,
   deleteById,
+  findByParam,
 };
-
-// sequelize.sync()
-//   .then(() => Product.create({
-//     name: 'product2',
-//     description: 'this is the second product'
-//   }).then(function(product2) {
-//       product2.setCategories([1, 2]);
-//   }));
-
-// function ProductDAO(connection) {
-//   this.connection = connection;
-// }
-//
-// ProductDAO.prototype.findAll = function findAll(callback) {
-//   const sql = 'SELECT p.id, p.name as product, p.description, ' +
-//     'GROUP_CONCAT(c.name SEPARATOR ", ") as categories ' +
-//     'FROM products p JOIN product_category pc ON p.id = pc.product_id ' +
-//     'JOIN categories c ON c.id = pc.category_id GROUP BY p.name ORDER BY p.name';
-//   this.connection.query(sql, callback);
-// };
-//
-// ProductDAO.prototype.findById = function findById(id, callback) {
-//   this.connection.query('SELECT * FROM products WHERE id = ?', [id], callback);
-// };
-//
-// ProductDAO.prototype.findByName = function findByName(name, callback) {
-//   this.connection.query('SELECT * FROM products WHERE name LIKE ?', `%${[name]}%`, callback);
-// };
-//
-// module.exports = function getProductDAO() {
-//   return ProductDAO;
-// };
-
-
-// const findAll = (cb) => {
-//   sequelize.sequelizeConnection.sync().then(() => {
-//     sequelize.Product.findAll({
-//       include: [sequelize.Category],
-//     }).then((products) => {
-//       console.log(JSON.parse(JSON.stringify(products)));
-//       //console.log(products[0].get('name'));
-//       //console.log(products[0].get('categories')[0].get('name'));
-//       cb(JSON.parse(JSON.stringify(products)));
-//     });
-//   });
-// };
