7ca239fd4dbf071ebdd9540038633739f18bc54a
Clara Tersi <claratersi@gmail.com>
2017-10-07 14:43:21 -0300

    product form

--- app/public/scripts/index.js
+++ app/public/scripts/index.js
@@ -19,4 +19,40 @@ $(document).ready(() => {
       { data: 'name' },
     ],
   });
+
+  $('#productModal').on('shown.bs.modal', () => {
+    $.ajax({ url: '/category',
+      success(results) {
+        results.forEach((result) => {
+          $('#categoriesMultipleSelect')
+            .append($('<option></option>')
+              .attr('value', result.id)
+              .text(result.name));
+        });
+      } });
+  });
+
+  $('#productModal').on('hidden.bs.modal', () => {
+    $('#categoriesMultipleSelect').html('');
+  });
+
+  $('#productForm').submit((event) => {
+    const selectedCategories = [];
+    $('#categoriesMultipleSelect :selected').each(function () {
+      selectedCategories.push($(this).val());
+    });
+    const formData = {
+      name: $('input[name=productName]').val(),
+      description: $('textarea[name=productDescription]').val(),
+      categories: selectedCategories.join(','),
+    };
+    $.ajax({
+      type: 'POST',
+      url: '/product',
+      data: formData,
+      dataType: 'json',
+      encode: true,
+    });
+    event.preventDefault();
+  });
 });
